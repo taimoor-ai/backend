b9135b0fff098651fc86aefcf297f859b8126f5d
taimoor-ai <taimooor786786@gmail.com>
2025-01-07 19:43:19 +0500

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,12 +1,15 @@
-const mysql=require("mysql2")
-const dotenv=require("dotenv")
-dotenv.config();
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
 const pool = mysql.createPool({
-    host:process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS, // replace with your MySQL password
-    database: process.env.DB_NAME, // replace with your MySQL database name
-  });
-  
-  const promisePool = pool.promise(); // This allows you to use async/await
-  module.exports=promisePool;
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
